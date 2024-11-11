06c3b033774431aadd3d95d9fcbe93b4944a9e99
SpiderMonkey <ascenzen11@gmail.com>
2024-11-11 14:23:00 +0530

    Add BoxModel component and update page layout

---
+++ app/models/box-model.tsx
@@ -0,0 +1,28 @@
+import { OrbitControls, Wireframe } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { motion } from "framer-motion-3d";
+
+export const BoxModel = () => {
+  return (
+    <>
+      <Canvas
+        style={{
+          width: "100vw",
+          height: "100vh",
+        }}
+        camera={{ position: [12, 12, 9] }}
+      >
+        <motion.mesh
+          animate={{
+            x: 12,
+            y: 12,
+          }}
+        >
+          <OrbitControls />
+          <torusGeometry />
+          <Wireframe />
+        </motion.mesh>
+      </Canvas>
+    </>
+  );
+};
